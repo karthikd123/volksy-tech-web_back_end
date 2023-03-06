4d56b52a2d4485cd3cd7a54bc8344645330c0d33
Your Name <you@example.com>
2023-03-06 05:55:20 +0000

    task1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,9 @@
-console.log('Welcome to Holberton School, what is your name?');
-// await input
-process.stdin.resume();
-// once input is entered
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}`);
-  // check if input is coming from the terminal
-  if (process.stdin.isTTY) {
-    process.exit();
-  } else {
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-  }
+  if (name) process.stdout.write(`Your name is: ${name}`);
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
