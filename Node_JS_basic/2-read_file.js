09b33cb2673cde9203d87b794530e49540437b05
Your Name <you@example.com>
2023-03-09 06:25:21 +0000

    task2.0

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,21 +1,50 @@
 const fs = require('fs');
 
-const countStudents = (aPath) => {
+function countStudents(filepath) {
   try {
-    let data = fs.readFileSync(aPath, 'utf8').toString().split('\n');
-    data = data.slice(1, data.length - 1);
-    console.log(`Number of students: NUMBER_OF_STUDENTS);
-    const subjects = {};
-    for (const row of data) {
-      const student = row.split(',');
-      if (!subjects[student[3]]) subjects[student[3]] = [];
-      subjects[student[3]].push(student[0]);
+    const csv = fs.readFileSync(filepath, { encoding: 'utf8' });
+    const headerArray = csv.split(/\r?\n|\n/);
+    const headers = headerArray[0].split(',');
+
+    // strip headers and convert to list of dicts
+    const dictList = [];
+    const noHeaderArray = headerArray.slice(1);
+    for (let i = 0; i < noHeaderArray.length; i += 1) {
+      const data = noHeaderArray[i].split(',');
+      if (data.length === headers.length) {
+        const row = {};
+        for (let j = 0; j < headers.length; j += 1) {
+          row[headers[j].trim()] = data[j].trim();
+        }
+        dictList.push(row);
+      }
     }
-    for (const subject in subjects) {
-      if (subject) console.log(`Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES .join(', ')}`);
-    }
-  } catch (error) {
+
+    // count and collect first names of students per field
+    let countCS = 0;
+    let countSWE = 0;
+    const studentsCS = [];
+    const studentsSWE = [];
+
+    dictList.forEach((element) => {
+      if (element.field === 'CS') {
+        countCS += 1;
+        studentsCS.push(element.firstname);
+      } else if (element.field === 'SWE') {
+        countSWE += 1;
+        studentsSWE.push(element.firstname);
+      }
+    });
+
+    const countStudents = countCS + countSWE;
+
+    // print statements
+    console.log(`Number of students: ${countStudents}`);
+    console.log(`Number of students in CS: ${countCS}. List: ${studentsCS.toString().split(',').join(', ')}`);
+    console.log(`Number of students in SWE: ${countSWE}. List: ${studentsSWE.toString().split(',').join(', ')}`);
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
-};
+}
+
 module.exports = countStudents;
