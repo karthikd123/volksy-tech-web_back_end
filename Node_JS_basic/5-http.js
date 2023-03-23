9962018711a545e3b140f216abe393ddd9e70830
Your Name <you@example.com>
2023-03-23 10:56:33 +0000

    H5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,36 +1,31 @@
 const http = require('http');
-
-const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const DATABASE = args[0];
-
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 1245;
+
+const file = process.argv[2];
 
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
-  const { url } = req;
-
-  if (url === '/') {
-    res.write('Hello Holberton School!');
-  } else if (url === '/students') {
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      const students = await countStudents(DATABASE);
+      const students = await countStudents(file);
       res.end(`${students.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
+    } catch (e) {
+      res.end(e.message);
     }
+    res.statusCode = 404;
+    res.end();
   }
-  res.statusCode = 404;
-  res.end();
 });
 
 app.listen(port, hostname, () => {
-  //   console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
