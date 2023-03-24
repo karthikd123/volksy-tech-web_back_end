c3b3a984504282785a5bd9bf8e56dc9efc6c8bd4
Your Name <you@example.com>
2023-03-24 05:34:33 +0000

    tsk5.1

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,62 +1,36 @@
 const http = require('http');
-const fs = require('fs');
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
-      try {
-        if (err) reject(Error('Cannot load the database'));
-        const totalData = data.split('\n').slice(1);
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
 
-        let csCount = 0;
-        let csStudents = '';
+const DATABASE = args[0];
 
-        let sweCount = 0;
-        let sweStudents = '';
+const hostname = '127.0.0.1';
+const port = 1245;
 
-        totalData.forEach((line) => {
-          const students = line.split(',');
-          if (students[3] === 'CS') {
-            csCount += 1;
-            csStudents += csStudents ? `, ${students[0]}` : students[0];
-          } else if (students[3] === 'SWE') {
-            sweCount += 1;
-            sweStudents += sweStudents ? `, ${students[0]}` : students[0];
-          }
-        });
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-        return resolve({
-          total: totalData.length,
-          csCount,
-          csStudents,
-          sweCount,
-          sweStudents,
-        });
-      } catch (err) { return reject(Error('Cannot load the database')); }
-    });
-  });
-}
+  const { url } = req;
 
-const app = http.createServer((req, res) => {
-  if (req.url === '/' && req.method === 'GET') {
+  if (url === '/') {
     res.write('Hello Holberton School!');
-    res.end();
-  } else if (req.url === '/students' && req.method === 'GET') {
-    countStudents('database.csv').then(
-      ({
-        total, csCount, csStudents, sweCount, sweStudents,
-      }) => {
-        res.write('This is the list of our students\n');
-        res.write(`Number of students: ${total}\n`);
-        res.write(
-          `Number of students in CS: ${csCount}. List: ${csStudents}\n`,
-        );
-        res.write(
-          `Number of students in SWE: ${sweCount}. List: ${sweStudents}\n`,
-        );
-      },
-    ).catch((err) => { throw err; });
-  } else { res.end(); }
-}).listen(1245);
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.statusCode = 404;
+  res.end();
+});
+
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
